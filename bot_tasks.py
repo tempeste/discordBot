d01acbd0fd9fe614babf841c8303398fb039e72d
tempeste <stanleyquekcp@gmail.com>
2024-01-28 16:45:32 +0800

    Fixed status message

--- bot_tasks.py
+++ bot_tasks.py
@@ -6,7 +6,7 @@ import utils
 async def update_bot_status(client):
     _, cpu_usage, mem_usage, server_status = await utils.check_palworld_server()
     status_message = f"CPU: {cpu_usage}, Memory: {mem_usage}, Status: {server_status}"
-    await client.change_presence(activity=discord.Game(name=server_status))
+    await client.change_presence(activity=discord.Game(name=status_message))
 
 def start_update_task(client):
     update_bot_status.start(client)
